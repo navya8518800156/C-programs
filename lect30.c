cb2ef740d315362c81730cdd6af0fb4fec9c0c78
navyapatel <patelnavya77064@gmail.com>
2025-10-30 16:02:34 +0530

    RCSN

---
+++ lect30.c
@@ -0,0 +1,43 @@
+// #include<stdio.h>
+// void sum (int a){
+//         if (a==0){
+//             return ;
+
+//         }
+//         printf ("%d",a);
+//           sum(a-1);
+
+//     }
+
+//         int main() {
+//             sum (3);
+//         }
+
+    // int sum (int arr[],int n){
+    //     if (n==0){
+    //         return 0;
+    //     }
+    //     return arr[n-1]+sum(arr,n-1);
+    // }
+                                                                                                               
+    // int main(){
+    //     int arr[3]={5,3,1};
+    //     int n=3;
+    //     int data= sum(arr,n);
+    //     printf("%d",data);
+    // }
+
+
+
+    // int power(int a){
+    //     if (a==0){
+    //         return 1;
+
+    //     }
+    //     return 2*power(a-1);
+
+    // }
+    // int main(){
+    //     int data =power(5);
+    //     printf("%d",data);
+    // }
